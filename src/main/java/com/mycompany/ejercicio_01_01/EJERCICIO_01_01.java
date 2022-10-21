ec4d3d02f52ffb59dc82041ce90448cf17e614a6
Portal Center <Portal Center@DESKTOP-52PINDA>
2022-10-21 10:21:30 -0500

    Terminado

---
+++ src/main/java/com/mycompany/ejercicio_01_01/EJERCICIO_01_01.java
@@ -0,0 +1,303 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Project/Maven2/JavaApp/src/main/java/${packagePath}/${mainClassName}.java to edit this template
+ */
+
+package com.mycompany.ejercicio_01_01;
+
+/**
+ *
+ * @author Juan Xavier Peralta Leon
+ */
+public class EJERCICIO_01_01 {
+
+    public static void main(String[] args) {
+        
+        var perro1 =new Perro();
+        perro1.nombre="Lola";
+        perro1.raza="Pastor Aleman";
+        perro1.edad=5;
+        perro1.peso="25 kilos";
+        
+        System.out.println("El perro se llama: "+perro1.nombre);
+        System.out.println("El perro es de raza: "+perro1.raza);
+        System.out.println("El perro tiene una edad de: "+perro1.edad+" años");
+        System.out.println("El perro pesa: "+perro1.peso);
+      
+        
+        
+        
+        var perro2 =new Perro();
+        perro2.nombre="Cholo";
+        perro2.raza="Dalmata";
+        perro2.edad=7;
+        perro2.peso="29 kilos";
+        
+       System.out.println("El perro se llama: "+perro2.nombre);
+       System.out.println("El perro es de raza: "+perro2.raza);
+       System.out.println("El perro tiene una edad de: "+perro2.edad+" años");
+       System.out.println("El perro pesa: "+perro2.peso);
+            
+        
+        
+        
+        var perro3 =new Perro();
+        perro3.nombre="Lucas";
+        perro3.raza="Labrador";
+        perro3.edad=4;
+        perro3.peso="19 kilos";
+        
+        System.out.println("El perro se llama: "+perro3.nombre);
+        System.out.println("El perro es de raza: "+perro3.raza);
+        System.out.println("El perro tiene una edad de: "+perro3.edad+" años");
+        System.out.println("El perro pesa: "+perro3.peso);
+        
+ 
+        
+        
+        
+        
+        
+        
+        
+        
+        var gato1 =new Gato();
+        gato1.nombre="Lucrecia";
+        gato1.raza="Persa";
+        gato1.edad=10;
+        gato1.peso="2 kg";
+        
+        System.out.println("El perro se llama: "+gato1.nombre);
+        System.out.println("El perro es de raza: "+gato1.raza);
+        System.out.println("El perro tiene una edad de: "+gato1.edad+" años");
+        System.out.println("El perro pesa: "+gato1.peso);
+        
+        
+        
+        
+        
+        var gato2 =new Gato();
+        gato2.nombre="Pelusa";
+        gato2.raza="Siames";
+        gato2.edad=4;
+        gato2.peso="5 kg";
+        
+        System.out.println("El perro se llama: "+gato2.nombre);
+        System.out.println("El perro es de raza: "+gato2.raza);
+        System.out.println("El perro tiene una edad de: "+gato2.edad+" años");
+        System.out.println("El perro pesa: "+gato2.peso);
+        
+        
+        
+        
+        
+        var gato3 =new Gato();
+        gato3.nombre="Nieve";
+        gato3.raza="Siberiano";
+        gato3.edad=5;
+        gato3.peso="4 kg";
+        
+        System.out.println("El perro se llama: "+gato3.nombre);
+        System.out.println("El perro es de raza: "+gato3.raza);
+        System.out.println("El perro tiene una edad de: "+gato3.edad+" años");
+        System.out.println("El perro pesa: "+gato3.peso);
+        
+        
+        
+        
+        
+        
+        
+        
+          
+        var persona1 =new Persona();
+        persona1.nombre="Pedro";
+        persona1.apellido="Martinez";
+        persona1.edad=21;
+        persona1.peso="74kg";
+        
+        System.out.println("La persona se llama: "+persona1.nombre);
+        System.out.println("La persona es de apellido: "+persona1.apellido);
+        System.out.println("La persona tiene: "+persona1.edad+" años");
+        System.out.println("La persona pesa: "+persona1.peso);
+        
+        
+        
+        
+        
+        var persona2 =new Persona();
+        persona2.nombre="Santiago";
+        persona2.apellido="Torres";
+        persona2.edad=21;
+        persona2.peso="69kg";
+        
+        System.out.println("La persona se llama: "+persona2.nombre);
+        System.out.println("La persona es de apellido: "+persona2.apellido);
+        System.out.println("La persona tiene: "+persona2.edad+" años");
+        System.out.println("La persona pesa: "+persona2.peso);
+        
+        
+        
+        
+        
+        var persona3 =new Persona();
+        persona3.nombre="Carlos";
+        persona3.apellido="Andrade";
+        persona3.edad=21;
+        persona1.peso="80kg";
+             
+        System.out.println("La persona se llama: "+persona3.nombre);
+        System.out.println("La persona es de apellido: "+persona3.apellido);
+        System.out.println("La persona tiene: "+persona3.edad+" años");
+        System.out.println("La persona pesa: "+persona3.peso);
+        
+     
+        
+        
+        
+        
+        
+        
+        
+        var carro1 =new Carro();
+        carro1.marca="Ford";
+        carro1.color="Marron";
+        carro1.año=1979;
+        carro1.peso="3.4 toneladas";
+        
+        System.out.println("El carro es de marca: "+carro1.marca);
+        System.out.println("El carro es de color: "+carro1.color);
+        System.out.println("El carro es del año: "+carro1.año);
+        System.out.println("El carro pesa: "+carro1.peso);
+        
+        
+        
+        
+        
+        var carro2 =new Carro();
+        carro2.marca="Chevrolet";
+        carro2.color="Blanco";
+        carro2.año=1999;
+        carro2.peso="2.9 toneladas";
+        
+        System.out.println("El carro es de marca: "+carro2.marca);
+        System.out.println("El carro es de color: "+carro2.color);
+        System.out.println("El carro es del año: "+carro2.año);
+        System.out.println("El carro pesa: "+carro2.peso);
+        
+        
+        
+        
+        
+        var carro3 =new Carro();
+        carro3.marca="Nizan";
+        carro3.color="Negro";
+        carro3.año=1979;
+        carro3.peso="5.5 toneladas";
+        
+        System.out.println("El carro es de marca: "+carro3.marca);
+        System.out.println("El carro es de color: "+carro3.color);
+        System.out.println("El carro es del año: "+carro3.año);
+        System.out.println("El carro pesa: "+carro3.peso);
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        var laptop1 =new Laptop();
+        laptop1.marca="HP";
+        laptop1.color="Azul";
+        laptop1.año=2020;
+        laptop1.peso="2.5kg";
+        
+        System.out.println("La laptop es de marca: "+laptop1.marca);
+        System.out.println("La laptop es de color: "+laptop1.color);
+        System.out.println("La laptop es del año: "+laptop1.año);
+        System.out.println("La laptop pesa: "+laptop1.peso);
+        
+        
+        
+        
+        
+        var laptop2 =new Laptop();
+        laptop2.marca="Apple";
+        laptop2.color="HP";
+        laptop2.año=2020;
+        laptop2.peso="4kg";
+        
+        System.out.println("La laptop es de marca: "+laptop2.marca);
+        System.out.println("La laptop es de color: "+laptop2.color);
+        System.out.println("La laptop es del año: "+laptop2.año);
+        System.out.println("La laptop pesa: "+laptop2.peso);
+        
+        
+        
+        
+        
+        var laptop3 =new Laptop();
+        laptop3.marca="Lenovo";
+        laptop3.color="HP";
+        laptop3.año=2020;
+        laptop3.peso="6kg";
+        
+        System.out.println("La laptop es de marca: "+laptop2.marca);
+        System.out.println("La laptop es de color: "+laptop2.color);
+        System.out.println("La laptop es del año: "+laptop2.año);
+        System.out.println("La laptop pesa: "+laptop2.peso);
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        var telefono1 =new Telefono();
+        telefono1.marca="Xiomi";
+        telefono1.color="Negro";
+        telefono1.año=2017;
+        telefono1.peso="148 gramos";
+        
+        System.out.println("El teléfono es de marca: "+telefono1.marca);
+        System.out.println("El teléfono es de color: "+telefono1.color);
+        System.out.println("El teléfono es del año: "+telefono1.año);
+        System.out.println("El teléfono pesa: "+telefono1.peso);
+        
+        
+        
+        
+        
+        var telefono2 =new Telefono();
+        telefono2.marca="Iphone";
+        telefono2.color="Gris";
+        telefono2.año=2017;
+        telefono2.peso="130 gramos";
+        
+        System.out.println("El teléfono es de marca: "+telefono2.marca);
+        System.out.println("El teléfono es de color: "+telefono2.color);
+        System.out.println("El teléfono es del año: "+telefono2.año);
+        System.out.println("El teléfono pesa: "+telefono2.peso);
+        
+        
+        
+        
+        var telefono3 =new Telefono();
+        telefono3.marca="Samsung";
+        telefono3.color="Azul";
+        telefono3.año=2017;
+        telefono3.peso="170 gramos";
+        
+        System.out.println("El teléfono es de marca: "+telefono2.marca);
+        System.out.println("El teléfono es de color: "+telefono2.color);
+        System.out.println("El teléfono es del año: "+telefono2.año);
+        System.out.println("El teléfono pesa: "+telefono2.peso);{
+        
+    }
+                
+    }
+}
---
+++ src/main/java/com/mycompany/ejercicio_01_01/Laptop.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.ejercicio_01_01;
+
+/**
+ *
+ * @author Portal Center
+ */
+public class Laptop {
+    
+    public String marca;
+    public String color;
+    public int año;
+    public String peso;
+    
+}
